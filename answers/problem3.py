d7244f57364efb06f2405ae512c8785830b1daa0
Archit Raj <raj.ar@northeastern.edu>
2023-09-22 22:08:54 -0400

    Checked the comments for problem3.py

--- answers/problem3.py
+++ answers/problem3.py
@@ -10,9 +10,11 @@
 # Your answer should include the new model definition as well as the code used to calculate the new values and store them in the database.
 
 
+# Import required modules and functions from psycopg2
 import psycopg2
 from psycopg2 import sql
 
+# Establish a connection to the PostgreSQL database
 connection = psycopg2.connect(
     dbname="weather_data",
     user="postgres",
@@ -22,40 +24,51 @@ connection = psycopg2.connect(
 )
 cursor = connection.cursor()
 
-# Get distinct station IDs and years
+# Query to fetch distinct station IDs and years from weather_data table
 cursor.execute("""
     SELECT DISTINCT station, EXTRACT(YEAR FROM date)
     FROM weather_data
 """)
+# Store the fetched data for further processing
 stations_and_years = cursor.fetchall()
 
+# Create a new table `weather_stats` to store aggregated weather statistics
+# This table includes average max and min temperatures and total precipitation for each station per year
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS weather_stats (
-    id SERIAL PRIMARY KEY,
-    station VARCHAR(255),
-    year INT,
-    avg_max_temp FLOAT,
-    avg_min_temp FLOAT,
-    total_precipitation FLOAT
+    id SERIAL PRIMARY KEY,         # Auto-incrementing ID
+    station VARCHAR(255),         # Station identifier
+    year INT,                      # Year of the recorded data
+    avg_max_temp FLOAT,            # Average of maximum temperatures
+    avg_min_temp FLOAT,            # Average of minimum temperatures
+    total_precipitation FLOAT      # Total precipitation (scaled)
 )
 ''')
 
+# Loop through each unique combination of station and year
 for station, year in stations_and_years:
-    # Calculate statistics
+    
+    # For each combination, calculate the following statistics:
+    # - Average of maximum temperatures (excluding invalid readings represented by -9999)
+    # - Average of minimum temperatures (excluding invalid readings represented by -9999)
+    # - Total precipitation (excluding invalid readings and scaled by dividing by 10)
     cursor.execute(sql.SQL("""
         INSERT INTO weather_stats (station, year, avg_max_temp, avg_min_temp, total_precipitation)
         SELECT 
-            %s,
-            %s,
-            AVG(max_temp) FILTER (WHERE max_temp > -9999),
-            AVG(min_temp) FILTER (WHERE min_temp > -9999),
-            SUM(precipitation) FILTER (WHERE precipitation > -9999) / 10
+            %s,            # Current station
+            %s,            # Current year
+            AVG(max_temp) FILTER (WHERE max_temp > -9999),  # Filter out invalid max temperatures
+            AVG(min_temp) FILTER (WHERE min_temp > -9999),  # Filter out invalid min temperatures
+            SUM(precipitation) FILTER (WHERE precipitation > -9999) / 10  # Filter and scale precipitation
         FROM 
             weather_data 
         WHERE 
-            station = %s AND EXTRACT(YEAR FROM date) = %s
+            station = %s AND EXTRACT(YEAR FROM date) = %s  # Filter by current station and year
     """), (station, year, station, year))
 
+# Commit the database changes
 connection.commit()
+
+# Close the cursor and connection to free up resources
 cursor.close()
 connection.close()
